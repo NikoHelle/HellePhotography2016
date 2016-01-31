7fe3811167e31135031af7839045d9f863761f5d
NikoHelle <niko.helle@hyperactive.fi>
2016-01-31 16:21:31 +0200

    s

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
       <script>try{Typekit.load();}catch(e){}</script>
   </head>
   <body>
-   <header>
+   <header id="header">
        <nav class="main">
            <a href="/muut">Muut</a>
            <a href="/haat">Häät</a>
@@ -20,15 +20,16 @@
            <a href="/koirat">Koirat</a>
        </nav>
        <nav class="contact">
-           <a href="/hinnat">Hinnasto</a>
-           <a href="/hinnat">Ota yhteyttä</a>
+           <a href="#prices">Hinnasto</a>
+           <a href="#contact">Ota yhteyttä</a>
            <a href="/hinnat">Facebook</a>
            <a href="/hinnat">Instagram</a>
            <a href="/hinnat">Pinterest</a>
        </nav>
    </header>
    <section>
-       <h1>Lapset</h1><p>Lapsikuvaus on kuvaajan parasta aikaa! Kuvaan paljon 1-vuotiaita, joita hyvin harvoin ujostuttaa ja aina löytyy jotain millä heidät saa hymyilemään. Viimeistään saippuakuplakone saa silmät loistamaan ja iloiset ilmeet jokaiseen kuvaan!</p>
+       <h1>Lapsikuvaus<span>on kuvaajan parasta aikaa!</span></h1>
+       <p>Kuvaan paljon 1-vuotiaita, joita hyvin harvoin ujostuttaa ja aina löytyy jotain millä heidät saa hymyilemään. Viimeistään saippuakuplakone saa silmät loistamaan ja iloiset ilmeet jokaiseen kuvaan!</p>
    </section>
    <section>
        <?
@@ -37,17 +38,39 @@
        ?>
    </section>
    <footer>
-       <div class="left">
-           <h1>Helle Photography</h1>
+       <div class="left" id="contact">
+           <form>
+            <h3>Ota yhteyttä</h3>
+            <input type="email" name="email" value="" placeholder="Sähköposti" />
+            <textarea name="message" placeholder="Viestisi"></textarea>
+            <input type="hidden" name="verification" value="23oi4dslf"/>
+            <a href="#" id="sendForm">Lähetä</a>
+           </form>
+           <h3>Kuvaaja</h3>
+           <div class="photographer">
+               <p>Niko Helle</p>
+               <p>niko@hellephotography.com</p>
+               <p>050-3606274</p>
+           </div>
        </div>
-       <div class="right">
+       <div class="right" id="prices">
+           <h3>Hinnasto</h3>
            <p>
-               Koirien kuvaaminen on mahtavaa puuhaa.
+               Studiokuvauksen hinta on <strong>150€</strong>, johon kuuluu puolitoista tuntia kuvausaikaa ja <strong>12</strong> finalisoitua kuvaa. Nämä kuvat saat itse valita studiossa otettujen joukosta, joita on tavallisesti <strong>150-300</strong> - riippuen mallin paikallaan pysymisestä. :)
+               Kuvien finalisoinnissa säädetään värit ja valaistus sekä korjataan esimerkiksi kuolat, taustan epätasaisuudet ja muut asiat, jotka eivät kuulu kuvaan.
            </p>
            <p>
-               Kiinnostuitko?
+               Kuvaan mielelläni myös studion ulkopuolella ja kalustoni on liikuteltavissa minne tahansa.
            </p>
+           <p>
+               Hääkuvausten hinta vaihtelee kuvaustyyppien mukaan. Portrettien ottaminen on järjestelyiden ja jälkikäsittelyn kannalta aikaavievintä, joten ne ovat siksi kallein osuus. Koko päivän kuvaus (10h) on <strong>900€</strong> ja pelkkä kirkko + portretit ovat <strong>500€</strong>.
+           </p>
+           <p> Hinnat sisältävät arvonlisäveron 24%.</p>
+           <p><strong>Ota yhteyttä ja varaa aika rakkaittesi ikuistamiseksi!</strong></p>
+
        </div>
+       <div class="divider"></div>
+       <a href="#header" class="logo"><img src="img/sprites/logo_text_bw.png" alt="Helle Photography"/></a>
 
    </footer>
 
