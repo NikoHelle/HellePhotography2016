adce50fe929574a5e946551181fdec49c0c20939
Niko Helle <niko.helle@hyperactive.fi>
2016-02-10 16:32:12 +0200

    sdfsdfs

--- js/init.js
+++ js/init.js
@@ -5,52 +5,22 @@ requirejs.config({
     paths: {
         jquery                   : "vendor/jquery-1.9.1.min",
         modernizr                : "vendor/modernizr",
-        underscore                : "vendor/underscore-min",
+        underscore               : "vendor/underscore-min",
         App                      : "App",
         Utils                    : "utils/Utils",
         EventHandler             : "events/EventHandler",
-        domReady                 : "vendor/domReady"
+        domReady                 : "vendor/domReady",
+        raf                      : "vendor/raf"
     }
 });
 
-require(["jquery", "modernizr","domReady"], function($,modernizr,domReady)
+require(["jquery", "modernizr","domReady","raf"], function($,modernizr,domReady,raf)
 {
     domReady(function () {
         require(["App"],
             function(App)
             {
-
-                if(!window.requestAnimationFrame) {
-
-                    var vendors = ['ms', 'moz', 'webkit', 'o'];
-                    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-                        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-                        window.cancelAnimationFrame =
-                            window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-                    }
-
-                    if (!window.requestAnimationFrame) {
-                        var lastFrameTime = 0;
-                        window.requestAnimationFrame = function(callback, element) {
-                            var currTime = new Date().getTime();
-                            var timeToCall = Math.max(0, 16 - (currTime - lastFrameTime));
-                            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                                timeToCall);
-                            lastFrameTime = currTime + timeToCall;
-                            return id;
-                        };
-                    }
-
-                    if (!window.cancelAnimationFrame) {
-                        window.cancelAnimationFrame = function(id) {
-                            clearTimeout(id);
-                        };
-                    }
-
-                }
                 window.app = new App();
-
-
             }
         );
     })
