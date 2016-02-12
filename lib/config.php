6b5b1b58e24155355f1082de42dbd9e25f6ee684
NikoHelle <niko.helle@hyperactive.fi>
2016-02-13 01:04:52 +0200

    sfsdfdfs

--- lib/config.php
+++ lib/config.php
@@ -48,7 +48,7 @@ $config->sets["lapset"]->title = "Lapsikuvaus";
 $config->sets["lapset"]->subtitle = "on kuvaajan parasta aikaa!";
 $config->sets["lapset"]->description = "<p>Pienten lasten kuvaaminen on parasta! Kuvaan paljon 1- ja 2-vuotiaita, joita hyvin harvoin ujostuttaa. Viimeistään saippuakuplakone saa silmät loistamaan ja iloiset ilmeet jokaiseen kuvaan! <a href='https://facebook.com/hellephotography' target='_blank'>Facebook-sivultani</a> saat tietoa erityisistä <strong>lapsikuvauspäivistä</strong>, jolloin on reilu alennus kuvauksista. Kannattaa siis likettää sitä!</p>";
 $config->sets["lapset"]->metaDescription = "Pienten lasten kuvaaminen on parasta! Kuvaan paljon 1- ja 2-vuotiaita, joita hyvin harvoin ujostuttaa. Viimeistään saippuakuplakone saa silmät loistamaan ja iloiset ilmeet jokaiseen kuvaan!";
-$config->sets["lapset"]->metaDescription .= $config->sets["etusivu"]->metaDescription;
+$config->sets["lapset"]->metaDescription .= " ".$config->sets["etusivu"]->metaDescription;
 
 $config->sets["koirat"] = (object) array();
 $config->sets["koirat"]->setId = "72157649851892598";
@@ -59,7 +59,7 @@ $config->sets["koirat"]->title = "Koirakuvaus";
 $config->sets["koirat"]->subtitle = "on perheenjäsenen ikuistamista";
 $config->sets["koirat"]->description = "<p>Koirien kuvaamisesta pidän todella paljon! He eivät ujostele, näytä mielestään kaameilta tai yritä duckfacea. Ja jos näyttävät tyhmältä, se on hyvä tässä tapauksessa. Kuvaajana olen kärsivällinen ja osaan käsitellä koiria sekä päästää ääniä, joilla korvat nousee ja ilme kirkastuu! Kannattaa likettää <a href='https://facebook.com/hellephotography' target='_blank'>Facebook-sivujani</a>, jotta saat tietoa erityisistä <strong>koirakuvauspäivistä</strong>!</p>";
 $config->sets["koirat"]->metaDescription = "Koirien kuvaamisesta pidän todella paljon, sillä he eivät ujostele, näytä mielestään koskaan kaameilta tai yritä duckfacea. Kuvaajana olen kärsivällinen ja osaan käsitellä koiria sekä päästää ääniä, joilla korvat nousee ja ilme kirkastuu! ";
-$config->sets["koirat"]->metaDescription .= $config->sets["etusivu"]->metaDescription;
+$config->sets["koirat"]->metaDescription .= " ".$config->sets["etusivu"]->metaDescription;
 
 $config->sets["potretit"] = (object) array();
 $config->sets["potretit"]->setId = "72157663008697505";
@@ -70,7 +70,7 @@ $config->sets["potretit"]->title = "Henkilökuvaus";
 $config->sets["potretit"]->subtitle = " on paljon enemmän kuin selfie";
 $config->sets["potretit"]->description = "<p>Hyviä kuvia tarvitaan nykypäivänä moneen tarkoitukseen ja muutenkin selfieiden sijaan kannattaa ehdottomasti ikuistaa itsensä ammattilaisen luona joko yksin tai rakkaan ihmisen kanssa. Kuvauksiin voi tulla myös kaverin kanssa jolloin lasku ja otetut kuvat jaetaan molemmille! Älä ujostele vaan <a href=\"#contact\">ota yhteyttä!</a></p>";
 $config->sets["potretit"]->metaDescription = "Hyviä kuvia tarvitaan nykypäivänä moneen tarkoitukseen ja muutenkin selfieiden sijaan kannattaa ehdottomasti ikuistaa itsensä ammattilaisen luona joko yksin tai rakkaan ihmisen kanssa. Kuvauksiin voi tulla myös kaverin kanssa jolloin lasku ja otetut kuvat jaetaan molemmille! Älä ujostele vaan ota yhteyttä!";
-$config->sets["potretit"]->metaDescription .= $config->sets["etusivu"]->metaDescription;
+$config->sets["potretit"]->metaDescription .= " ".$config->sets["etusivu"]->metaDescription;
 
 $config->sets["maailma"] = (object) array();
 $config->sets["maailma"]->setId = "72157664174695212";
@@ -81,7 +81,7 @@ $config->sets["maailma"]->title = "Maailma";
 $config->sets["maailma"]->subtitle = "kameran silmin";
 $config->sets["maailma"]->description = "<p>Onneksi digimaailmassa saa kuvata niin paljon kuin sielu sietää ja tilaa riittää muistikortilla, sillä on olemassa niin paljon upeita paikkoja kuten Islanti, Nepal, Etelä-Afrikka, Azorit, Brasilia, Himalaja, NYC, Bahama, Berliini ja muu Euroooppa kuten ihan oma Suomemme, jossa on uskomattoman kaunis luonto.</p>";
 $config->sets["maailma"]->metaDescription = "Onneksi digimaailmassa saa kuvata niin paljon kuin sielu sietää ja tilaa riittää muistikortilla, sillä on olemassa niin paljon upeita paikkoja kuten Islanti, Nepal, Etelä-Afrikka, Azorit, Brasilia, Himalaja, NYC, Bahama, Berliini ja muu Euroooppa kuten ihan oma Suomemme, jossa on uskomattoman kaunis luonto.";
-$config->sets["maailma"]->metaDescription .= $config->sets["etusivu"]->metaDescription;
+$config->sets["maailma"]->metaDescription .= " ".$config->sets["etusivu"]->metaDescription;
 
 $config->sets["haat"] = (object) array();
 $config->sets["haat"]->setId = "72157647937571103";
@@ -92,7 +92,7 @@ $config->sets["haat"]->title = "Hääkuvaus";
 $config->sets["haat"]->subtitle = "ikuistaa rakkaat muistot";
 $config->sets["haat"]->description = "<p>Häät on ilon ja rakkauden juhla, joka pitää ikuistaa ammattilaistaidolla. Hämärät kirkot ja nopeat tilanteet vaativat kännykän sijaan oikean kameran. Häissä on aina upea tunnelma jota on mahtava ikuistaa!</p>";
 $config->sets["haat"]->metaDescription = "Häät on ilon ja rakkauden juhla, joka pitää ikuistaa ammattilaistaidolla. Hämärät kirkot ja nopeat tilanteet vaativat kännykän sijaan oikean kameran. Häissä on aina upea tunnelma jota on mahtava ikuistaa!";
-$config->sets["haat"]->metaDescription .= $config->sets["etusivu"]->metaDescription;
+$config->sets["haat"]->metaDescription .= " ".$config->sets["etusivu"]->metaDescription;
 
 $menu = array("lapset","koirat");
 
